dee1aa9a4ce69b049c37881a77de300678cdf406
Sacha Sedriks <sachased@gmail.com>
2020-06-06 14:21:05 -0700

    init Clock timer exercise: going from props-based functional component with external setInterval to ReactDOM.render to state-based (class) component with lifecycle to set (and clear) Interval for re-render.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, {useState} from 'react';
 import './App.css';
 import ChildButtons from './ChildButtons';
+import Clock from './Clock';
+
 
 // newer version using Hooks for state
 // see: https://reactjs.org/docs/hooks-state.html
@@ -42,15 +44,22 @@ class App extends React.Component {
     const { count } = this.state;
     return (
       <div className='App'>
-        The count is {count}
-        <div>   
-          <ChildButtons 
-            // method 1
-            // onClick={(count) => {this.setCount(count)}} 
-            // method 2 (^^^ why the tendency to use method 1?)
-            onClick={this.setCount} 
-            count={count} 
-          />
+
+        <div className='Counter'>
+          The count is {count}
+          <div>   
+            <ChildButtons 
+              // method 1
+              // onClick={(count) => {this.setCount(count)}} 
+              // method 2 (^^^ why the tendency to use method 1?)
+              onClick={this.setCount} 
+              count={count} 
+              />
+          </div>
+        </div>
+
+        <div className='Clock'>
+          <Clock date={new Date()}/>
         </div>
       </div>
     )
---
+++ src/Clock.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+export default class Clock extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      date: new Date(),
+    }
+  }
+
+  tick() {
+    this.setState({
+      date: new Date(),
+    });
+  }
+  
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}</h2>
+      </div>
+    )
+  }
+}
+
+// export default function Clock(props) {
+//   return (
+//     <div>
+//       <h1>Hello, world!</h1>
+//       <h2>It is {props.date.toLocaleTimeString()}.</h2>
+//     </div>
+//   );
+// }
