f6d501e044e8a3860d33bb453e2ec08b5469e3ac
Sacha Sedriks <sachased@gmail.com>
2020-06-07 22:21:27 -0700

    updated/resolved uuid issue in NumberListApp.

--- src/NumberList/NumberListApp.js
+++ src/NumberList/NumberListApp.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import uuid from 'react-uuid'; // NOTE: using react-uuid to generate 128bit Universal Unique Identifier (UUID) !!! =D
-// BUT IS IT WORKING????
-// esLint: "Could not find a declaration file for module 'react-uuid'""
 
 
 export default function NumberList(props) {
   const numbers = props.numbers;
   const listItems = numbers.map((number) =>
-    <li key={uuid}>{number}</li>    // NOTE: using react-uuid to generate 128bit Universal Unique Identifier (UUID) !!! =D
+    <li key={uuid()}>{number}</li>    // NOTE: using react-uuid to generate 128bit Universal Unique Identifier (UUID) !!! =D
     )
   return (
     <ul>{listItems}</ul>
